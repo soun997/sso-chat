44535a943855a5167a8e4efbe8416b13f79e9d2d
sypark <soun997@gmail.com>
2025-11-05 05:11:18 +0900

    init: 프로젝트 시작

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { AppController } from './app.controller';
+import { ChatsModule } from './chats/chats.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    ChatsModule,
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule implements NestModule {
+  configure() {
+    const DEBUG = process.env.MODE === 'dev' ? true : false;
+    mongoose.set('debug', DEBUG);
+  }
+}
